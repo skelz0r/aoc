d34aa62cc66685331fce406bc2d602d467985681
Loïc Delmaire <loic.delmaire@gmail.com>
2021-12-07 08:08:36 +0100

    Day 7

---
+++ 7/part2.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+data = File.read(
+  File.join(
+    File.dirname(__FILE__),
+    './example.txt'
+  )
+).split(',').map(&:to_i)
+
+min, max = data.min, data.max
+
+results = []
+
+min.upto(max) do |i|
+  tmp = data.dup
+
+  tmp.map! do |v|
+    (1..(v - i).abs).sum
+  end
+
+  # p "For #{i} => #{tmp.sum}"
+
+  results << tmp.sum
+end
+
+p results.min
